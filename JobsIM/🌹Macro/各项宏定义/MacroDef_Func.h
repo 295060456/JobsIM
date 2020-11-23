8405bcb223c9cdf7bac93efbbbbc0009944de0d5
Jobs <lg295060456@gmail.com>
2020-11-23 10:59:21 +0700

    更新获取window的方法

--- JobsIM/🌹Macro/各项宏定义/MacroDef_Func.h
+++ JobsIM/🌹Macro/各项宏定义/MacroDef_Func.h
@@ -13,7 +13,13 @@
 static inline UIWindow * getMainWindow(){
     UIWindow *window = nil;
     if (@available(iOS 13.0, *)) {
-        window = [JobsIMSceneDelegate sharedInstance].window;
+//        window = [JobsIMSceneDelegate sharedInstance].window;
+        for (UIWindowScene* windowScene in [UIApplication sharedApplication].connectedScenes){
+            if (windowScene.activationState == UISceneActivationStateForegroundActive){
+                window = windowScene.windows.firstObject;
+                break;
+            }
+        }
     }else{
         window = UIApplication.sharedApplication.delegate.window;
 //        [UIApplication sharedApplication].keyWindow
