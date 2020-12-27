5830c21e40950b68ea8978a6758932df2f05f6c2
Jobs <lg295060456@gmail.com>
2020-12-27 20:51:01 +0700

    no message

--- JobsIM/🌹Macro/第三方头文件管理/手动导入的第三方/Manual_Add_ThirdParty.h
+++ JobsIM/🌹Macro/第三方头文件管理/手动导入的第三方/Manual_Add_ThirdParty.h
@@ -49,6 +49,7 @@
 #import "UIViewController+TZLocationManager.h"
 #import "UIViewController+NavigationBar.h"
 #import "UIViewController+EmptyData.h"
+#import "UIViewController+XLBubbleTransition.h"
 //UITableViewHeaderFooterView
 #import "ViewForTableViewFooter.h"
 #import "ViewForTableViewHeader.h"
