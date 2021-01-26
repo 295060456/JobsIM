1b410d6b4a628dc320faffd0eab127a8d001bd10
Jobs <lg295060456@gmail.com>
2021-01-26 20:02:04 +0700

    no message

--- JobsIM/🇨🇳ServiceLogic/ViewController@4/JobsMarqueeView/JobsMarqueeView.m
+++ JobsIM/🇨🇳ServiceLogic/ViewController@4/JobsMarqueeView/JobsMarqueeView.m
@@ -14,6 +14,7 @@
 @interface JobsMarqueeView ()
 
 @property(nonatomic,strong)UIView *contentView;//帧动画的作用对象，所有的子控件都添加于此，而不是self
+@property(nonatomic,strong)UIImageView *imageView;
 @property(nonatomic,strong)UILabel *contentLab;
 @property(nonatomic,strong)CAKeyframeAnimation *keyFrame;
 
@@ -42,6 +43,7 @@
 -(void)richElementsWithModel:(JobsMarqueeConfig *_Nullable)marqueeConfig{
     
     self.keyFrame.duration = marqueeConfig.duration;
+    self.imageView.alpha = 1;
     self.contentLab.text = marqueeConfig.contentText;
     self.contentLab.font = marqueeConfig.font;
     [self.contentLab sizeToFit];
@@ -83,6 +85,19 @@
     }return _contentView;
 }
 
+-(UIImageView *)imageView{
+    if (!_imageView) {
+        _imageView = UIImageView.new;
+        _imageView.backgroundColor = kRedColor;
+        [self.contentView addSubview:_imageView];
+        [_imageView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.size.mas_equalTo(CGSizeMake(5, 5));
+            make.left.equalTo(self).offset(10);
+            make.centerY.equalTo(self);
+        }];
+    }return _imageView;
+}
+
 -(UILabel *)contentLab{
     if (!_contentLab) {
         _contentLab = UILabel.new;
@@ -90,7 +105,8 @@
         _contentLab.textColor = kBlueColor;
         [self.contentView addSubview:_contentLab];
         [_contentLab mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.top.bottom.left.equalTo(self);
+            make.top.bottom.equalTo(self);
+            make.left.equalTo(self.imageView.mas_right).offset(10);
         }];
     }return _contentLab;
 }
