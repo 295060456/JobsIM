34f5b72b9ddec9b974bd73e4c82d9a65b22cd97e
Jobs <lg295060456@gmail.com>
2021-05-04 20:31:17 +0700

    no message

--- JobsIM/🇨🇳ServiceLogic/Common/网络请求/App 的所有网络请求Api/NetworkingAPI.m
+++ JobsIM/🇨🇳ServiceLogic/Common/网络请求/App 的所有网络请求Api/NetworkingAPI.m
@@ -29,7 +29,7 @@
 /// 【只有Body参数、不需要错误回调】
 +(void)requestApi:(NSString *_Nonnull)requestApi
        parameters:(id _Nullable)parameters
-     successBlock:(MKDataBlock)successBlock{
+     successBlock:(MKDataBlock _Nullable)successBlock{
     
     NSLog(@"接口名：%@，请求参数打印 %@",requestApi,parameters);
     
@@ -55,8 +55,8 @@
 ///【只有Body参数、需要错误回调的】
 +(void)requestApi:(NSString *_Nonnull)requestApi
        parameters:(id _Nullable)parameters
-     successBlock:(MKDataBlock)successBlock
-     failureBlock:(MKDataBlock)failureBlock{
+     successBlock:(MKDataBlock _Nullable)successBlock
+     failureBlock:(MKDataBlock _Nullable)failureBlock{
     
     NSLog(@"接口名：%@，请求参数打印 %@",requestApi,parameters);
     
@@ -89,8 +89,8 @@
 /// 上传【图片】文件的网络请求 POST
 +(void)requestApi:(NSString *_Nonnull)requestApi
 uploadImagesParamArr:(NSArray *_Nullable)uploadImagesParamArr
-     successBlock:(MKDataBlock)successBlock
-     failureBlock:(MKDataBlock)failureBlock{
+     successBlock:(MKDataBlock _Nullable)successBlock
+     failureBlock:(MKDataBlock _Nullable)failureBlock{
 
     NSMutableArray *paramMutArr = nil;
     
@@ -116,8 +116,8 @@ uploadImagesParamArr:(NSArray *_Nullable)uploadImagesParamArr
 /// 上传【视频】文件的网络请求 POST
 +(void)requestApi:(NSString *_Nonnull)requestApi
 uploadVideosParamArr:(NSArray *_Nullable)uploadVideosParamArr
-     successBlock:(MKDataBlock)successBlock
-     failureBlock:(MKDataBlock)failureBlock{
+     successBlock:(MKDataBlock _Nullable)successBlock
+     failureBlock:(MKDataBlock _Nullable)failureBlock{
     
     NSMutableArray *paramMutArr = nil;
     
